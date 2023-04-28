aadca3d0301f7a4100e9723e63fbcd07128a5fbf
Ben Chavez <ben.m.chavez@gmail.com>
2023-04-28 11:02:11 -0500

    removed  from getInnerHtmlScript

--- src/contentScripts/getInnerHtmlScript.ts
+++ src/contentScripts/getInnerHtmlScript.ts
@@ -8,65 +8,63 @@ import {
   updateHighlights,
 } from '../utils/matchUtils';
 
-(function () {
-  if (window.myUniqueExtensionFlag) {
-    console.log('Content script already injected. Exiting...');
-    return;
+// (function () {
+// if (window.myUniqueExtensionFlag) {
+//   console.log('Content script already injected. Exiting...');
+//   return;
+// }
+
+// // Set the unique flag to indicate that the content script has been injected
+// window.myUniqueExtensionFlag = true;
+
+const state2 = {
+  currentIndex: undefined,
+  matchesObj: [] as string | any[],
+  tabId: undefined,
+  tabIndex: undefined,
+  matchesCount: undefined,
+  curMatchIndex: undefined,
+};
+
+console.log(new Date().toLocaleString());
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  // console.log("Rec'd msg:", message);
+
+  const { from, type, findValue, tabId, messageId } = message;
+
+  switch (`${from}:${type}`) {
+    case 'background:highlight':
+      state2.tabId = message.tabId;
+
+      await findAllMatches(state2, findValue);
+
+      // TODO: DRY
+      const serializedState2 = { ...state2 };
+
+      serializedState2.matchesObj = serializeMatchesObj(
+        serializedState2.matchesObj
+      );
+
+      sendResponse({
+        hasMatch: state2.matchesObj.length > 0,
+        serializedState2: serializedState2,
+      });
+
+      return true;
+    case 'background:next-match':
+      if (state2.matchesObj.length > 0) nextMatch(state2);
+      break;
+    case 'background:prev-match':
+      previousMatch(state2);
+      break;
+    case 'background:update-highlights':
+      updateHighlights(state2, message.prevIndex);
+      break;
+    default:
+      break;
   }
 
-  // Set the unique flag to indicate that the content script has been injected
-  window.myUniqueExtensionFlag = true;
-
-  const state2 = {
-    currentIndex: undefined,
-    matchesObj: [] as string | any[],
-    tabId: undefined,
-    tabIndex: undefined,
-    matchesCount: undefined,
-    curMatchIndex: undefined,
-  };
-
-  console.log(new Date().toLocaleString());
-
-  chrome.runtime.onMessage.addListener(
-    async (message, sender, sendResponse) => {
-      console.log("Rec'd msg:", message);
-
-      const { from, type, findValue, tabId, messageId } = message;
-
-      switch (`${from}:${type}`) {
-        case 'background:highlight':
-          state2.tabId = message.tabId;
-
-          await findAllMatches(state2, findValue);
-
-          // TODO: DRY
-          const serializedState2 = { ...state2 };
-
-          serializedState2.matchesObj = serializeMatchesObj(
-            serializedState2.matchesObj
-          );
-
-          sendResponse({
-            hasMatch: state2.matchesObj.length > 0,
-            serializedState2: serializedState2,
-          });
-
-          return true;
-        case 'background:next-match':
-          if (state2.matchesObj.length > 0) nextMatch(state2);
-          break;
-        case 'background:prev-match':
-          previousMatch(state2);
-          break;
-        case 'background:update-highlights':
-          updateHighlights(state2, message.prevIndex);
-          break;
-        default:
-          break;
-      }
-
-      return;
-    }
-  );
-})();
+  return;
+});
+// })();
