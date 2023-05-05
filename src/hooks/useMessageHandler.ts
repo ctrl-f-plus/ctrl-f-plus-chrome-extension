4ebb47b0b9bd0d32b7a2b86521ec316d7e37db76
Ben Chavez <ben.m.chavez@gmail.com>
2023-05-05 00:03:52 -0500

    code cleanup

--- src/hooks/useMessageHandler.ts
+++ src/hooks/useMessageHandler.ts
@@ -1,17 +1,4 @@
-// import { useEffect } from 'react';
-
-// type MessageHandler = (message: any, sender: any, sendResponse: any) => void;
-
-// export const useMessageHandler = (handler: MessageHandler) => {
-//   useEffect(() => {
-//     chrome.runtime.onMessage.addListener(handler);
-
-//     // Cleanup the event listener on unmount
-//     return () => {
-//       chrome.runtime.onMessage.removeListener(handler);
-//     };
-//   }, [handler]);
-// };
+// src/hooks/useMessageHandler.ts
 
 import { useEffect } from 'react';
 
@@ -19,14 +6,25 @@ type MessageHandler = (message: any, sender: any, sendResponse: any) => void;
 
 export const useMessageHandler = (messageHandler: MessageHandler) => {
   useEffect(() => {
-    const processMessage = (message: any, sender: any, sendResponse: any) => {
-      messageHandler(message, sender, sendResponse);
-    };
+    chrome.runtime.onMessage.addListener(messageHandler);
 
-    chrome.runtime.onMessage.addListener(processMessage);
-
+    // Cleanup the event listener on unmount
     return () => {
-      chrome.runtime.onMessage.removeListener(processMessage);
+      chrome.runtime.onMessage.removeListener(messageHandler);
     };
   }, [messageHandler]);
 };
+
+// ///////////////////////////////////////////
+
+// useEffect(() => {
+//   const processMessage = (message: any, sender: any, sendResponse: any) => {
+//     messageHandler(message, sender, sendResponse);
+//   };
+
+//   chrome.runtime.onMessage.addListener(processMessage);
+
+//   return () => {
+//     chrome.runtime.onMessage.removeListener(processMessage);
+//   };
+// }, [messageHandler]);
