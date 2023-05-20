4dab0e144dc47d0efb2fa3e0533597c226794866
Ben Chavez <ben.m.chavez@gmail.com>
2023-05-20 13:10:02 -0500

    continued removing unused createMessage functions

--- src/utils/messageUtils/createMessages.ts
+++ src/utils/messageUtils/createMessages.ts
@@ -1,36 +1,17 @@
 // src/utils/messageUtils/createMessages.ts
 
-import { Store } from '../../background/store';
 import { LayoverPosition } from '../../components/Layover';
 import {
-  GetAllMatchesMsg,
   HighlightMsg,
-  SwitchTabMsg,
   UpdateHighlightsMsg,
   UpdateLayoverPositionMsg,
-  UpdateStoreMsg,
-  UpdateTabStatesObjMsg,
 } from '../../types/message.types';
-import { SerializedTabState, ValidTabId } from '../../types/tab.types';
+import { ValidTabId } from '../../types/tab.types';
 
 /**
  * FROM: Background
  * TO: Content
  */
-export function createUpdateStoreMsg(store: Store): UpdateStoreMsg {
-  return {
-    from: 'background:store',
-    type: 'store-updated',
-    payload: {},
-  };
-}
-
-// export function createRemoveAllHighlightMatchesMsg(): RemoveAllHighlightMatchesMsg {
-//   return {
-//     from: 'background:backgroundUtils',
-//     type: 'remove-all-highlight-matches',
-//   };
-// }
 
 export function createHighlightMsg(
   findValue: string,
@@ -63,42 +44,6 @@ export function createUpdateHighlightsMsg(tabId: number): UpdateHighlightsMsg {
  * FROM: Content
  * TO: Background
  */
-export function createSwitchTabMsg(
-  serializedState: SerializedTabState,
-  prevIndex: number | undefined = undefined
-): SwitchTabMsg {
-  return {
-    from: 'content-script-match-utils',
-    type: 'switch-tab',
-    serializedState: serializedState,
-    prevIndex: prevIndex,
-  };
-}
-
-export function createSwitchTabMsg2(
-  serializedState: SerializedTabState,
-  prevIndex: number | undefined = undefined
-): SwitchTabMsg {
-  return {
-    from: 'content-script-match-utils',
-    type: 'switch-tab',
-    serializedState: serializedState,
-    prevIndex: prevIndex,
-  };
-}
-
-export function createUpdateTabStatesObjMsg(
-  serializedState: SerializedTabState
-): UpdateTabStatesObjMsg {
-  return {
-    from: 'content:match-utils',
-    type: 'update-tab-states-obj',
-    payload: {
-      serializedState,
-    },
-  };
-}
-
 export function createUpdateLayoverPositionMsg(
   newPosition: LayoverPosition
 ): UpdateLayoverPositionMsg {
@@ -108,14 +53,5 @@ export function createUpdateLayoverPositionMsg(
     payload: {
       newPosition,
     },
-  };
-}
-
-// export const createGetAllMatchesMsg = (findValue: string): GetAllMatchesMsg => {
-export function createGetAllMatchesMsg(findValue: string): GetAllMatchesMsg {
-  return {
-    from: 'content',
-    type: 'get-all-matches-msg',
-    payload: findValue,
   };
 }
